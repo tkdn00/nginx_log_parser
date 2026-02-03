521bb789e81f11ecfd6d572cea8daedaa5884ecd
tkdn00 <tarikppz12@gmail.com>
2026-02-03 12:26:54 +0000

    First commit

---
+++ parse.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+NGINX_LOG_FILE="nginx.log"
+PARSED_FOLDER="parsed_nginx"
+CSV_FILE="$PARSED_FOLDER/nginx.csv"
+
+mkdir -p "$PARSED_FOLDER"
+
+if [[ ! -f "$NGINX_LOG_FILE" ]]; then
+  exit 1
+fi
+
+echo "IP,Date,Method,URL_PROTOCOL,Status" > "$CSV_FILE"
+
+awk -F' ' '{print $1 "," substr($4,2) " " substr($5,1,5) "," substr($6,2) "," $7 " " substr($8,1,8) "," $9}' "$NGINX_LOG_FILE" >> "$CSV_FILE"
+
+
