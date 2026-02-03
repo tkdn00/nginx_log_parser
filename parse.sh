fd1312df6d12ff0a35ab044a3071a1ccb13b25d6
tkdn00 <tarikppz12@gmail.com>
2026-02-03 14:25:13 +0000

    parse.sh change

--- parse.sh
+++ parse.sh
@@ -16,7 +16,7 @@ awk -F' ' '{print $1 "," substr($4,2) " " substr($5,1,5) "," substr($6,2) "," $7
 
 if [ -d ".git" ]; then
     git add "$CSV_FILE"
-    git commit -m "Parsed logs $date"
+    git commit -m "Parsed logs"
 else
     echo "There is no git folder"
 fi
